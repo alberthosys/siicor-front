9b779f800cb38c3af7a72a1c0f4b83ee83992b92
alberthosys <albethosystemas@gmail.com>
2020-05-12 00:46:33 -0500

    Menu principal 11 Mayo

--- src/app/home-configuracion/home-configuracion.component.ts
+++ src/app/home-configuracion/home-configuracion.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Form, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home-configuracion',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-configuracion.component.css']
 })
 export class HomeConfiguracionComponent implements OnInit {
+  public router: boolean = false;
+  public dispositivoConsulta : Dispositivo;
+  public form:FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public  formBuilder:FormBuilder,
+  ){
+    this.form= formBuilder.group({
+      dispositivo:[null,Validators.compose([Validators.required])],
+      hostname:[null,Validators.compose([Validators.required])],
+      version:[null,Validators.compose([Validators.required])],
+      fechaModificacion:[null,Validators.compose([Validators.required])],
+    })
   }
 
+  ngOnInit() {
+    this.dispositivoConsulta={dispositivo:'Router',hostname:'D A T I C',version:'TL-WN725N',fechaModificacion:'2020/05/12'}
+    this.form.controls.dispositivo.setValue(this.dispositivoConsulta.dispositivo);
+    this.form.controls.hostname.setValue(this.dispositivoConsulta.hostname);
+    this.form.controls.version.setValue(this.dispositivoConsulta.version);
+    this.form.controls.fechaModificacion.setValue(new Date (this.dispositivoConsulta.fechaModificacion).toDateString());
+    console.log(this.dispositivoConsulta)
+  }
+
+}
+
+class Dispositivo {
+  dispositivo: string;
+  hostname: string;
+  version:string;
+  fechaModificacion: string;
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -14,12 +14,12 @@ export class MenuComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.menu.push({icono: 'home', nombre: 'Página Incial'})
-    this.menu.push({icono: 'arrow_forward', nombre: 'Direccionamiento'})
-    this.menu.push({icono: 'navigation', nombre: 'Enrutamiento'})
-    this.menu.push({icono: 'trip_origin', nombre: 'ACL'})
-    this.menu.push({icono: 'layers', nombre: 'VLAN'})
-    this.menu.push({icono: 'exit_to_app', nombre: 'Cerrar Sesión'})
+    this.menu.push({icono: 'https://image.flaticon.com/icons/png/512/553/553416.png', nombre: 'Página Incial'})
+    this.menu.push({icono: 'https://image.flaticon.com/icons/svg/724/724820.svg', nombre: 'Direccionamiento'})
+    this.menu.push({icono: 'https://image.flaticon.com/icons/svg/875/875625.svg', nombre: 'Enrutamiento'})
+    this.menu.push({icono: 'https://www.flaticon.com/premium-icon/icons/svg/2859/2859731.svg', nombre: 'ACL'})
+    this.menu.push({icono: 'https://image.flaticon.com/icons/svg/2919/2919581.svg', nombre: 'VLAN'})
+    this.menu.push({icono: 'https://image.flaticon.com/icons/svg/1828/1828490.svg', nombre: 'Cerrar Sesión'})
   }
 
   public cerrarSesion(item: Menu) {
