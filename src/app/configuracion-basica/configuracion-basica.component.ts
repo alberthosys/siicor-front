ec72cf91493235b2750badca79e0626d5bdfba19
alberthosys <albethosystemas@gmail.com>
2020-06-28 23:54:44 -0500

    24 de junio

--- src/app/configuracion-basica/configuracion-basica.component.ts
+++ src/app/configuracion-basica/configuracion-basica.component.ts
@@ -19,6 +19,9 @@ export class ConfiguracionBasicaComponent implements OnInit {
   public ventana: boolean = false;
   public formConsole: FormGroup;
   public formVty: FormGroup;
+  public showPasswords: boolean = false;
+  public showPasswords2: boolean = false;
+  public valMin: number = 0;
 
   constructor(
     public ruta: Router,
@@ -28,20 +31,25 @@ export class ConfiguracionBasicaComponent implements OnInit {
       enabled: [this.routerComand.enabled],
       config_terminal: [this.routerComand.config_terminal],
       line_console_0: [this.routerComand.line_console_0],
-      password: [null, Validators.compose([Validators.required])],
+      last_password: [null, Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      confirm_password: ['', Validators.compose([Validators.required])],
       login: [this.routerComand.login],
-      exit: [this.routerComand.exit],
-    })
+      exit: [this.routerComand.exit]
+    });
     this.formVty = formBuilder.group({
       enabled: [this.routerComand.enabled],
       config_terminal: [this.routerComand.config_terminal],
-      line_vty:[this.routerComand.line_vty],
+      line_vty: [this.routerComand.line_vty],
       cero: [false],
       uno: [false],
       dos: [false],
       tres: [false],
       cuatro: [false],
-      password: [null, Validators.compose([Validators.required])],
+      valorMinimo: [0, Validators.compose([Validators.required, Validators.min(0), Validators.pattern('[0-9]*')])],
+      valorMax: [null, Validators.compose([Validators.pattern('[0-9]*')])],
+      password: ['', Validators.compose([Validators.required])],
+      confirm_password: ['', Validators.compose([Validators.required])],
       login: [this.routerComand.login],
       exit: [this.routerComand.exit],
     })
@@ -64,4 +72,48 @@ export class ConfiguracionBasicaComponent implements OnInit {
       this.ruta.navigate([''])
     }
   }
+
+  actualizarLineConsole() {
+    let comands: string[] = [];
+    if (this.formConsole.controls.password.value != this.formConsole.controls.confirm_password.value) {
+      this.alerta.alertError("Revisa que todos los campos esten correctos!")
+      return
+    }
+    if (this.formConsole.valid) {
+      comands.push(this.formConsole.controls.last_password.value);
+      comands.push(this.routerComand.enabled);
+      comands.push(this.routerComand.config_terminal);
+      comands.push(this.routerComand.line_console_0);
+      comands.push(this.routerComand.password + this.formConsole.controls.password.value);
+      comands.push(this.routerComand.login);
+      comands.push(this.routerComand.exit);
+    } else {
+      this.alerta.alertError("Revisa que todos los campos esten correctos!")
+    }
+
+    console.log("comands->", comands)
+  }
+
+  actualizarVTY() {
+    let comands: string[] = [];
+  if(this.formVty.controls.valorMax.value!=null && this.formVty.controls.valorMax.value.trim()==''){
+    this.formVty.controls.valorMax.setValue(null);
+  }
+  if(this.formVty.controls.valorMax.value && this.formVty.controls.valorMinimo.value>=this.formVty.controls.valorMax.value){
+    this.alerta.alertError("Revisa que todos los campos esten correctos!")
+    return
+  }
+    if (this.formVty.valid) {
+      comands.push(this.routerComand.enabled);
+      comands.push(this.routerComand.config_terminal);
+      comands.push(this.routerComand.line_vty + (this.formVty.controls.valorMax.value? this.formVty.controls.valorMinimo.value + ' ' + this.formVty.controls.valorMax.value : this.formVty.controls.valorMinimo.value));
+      comands.push(this.routerComand.password + this.formConsole.controls.password.value);
+      comands.push(this.routerComand.login);
+      comands.push(this.routerComand.exit);
+    } else {
+      this.alerta.alertError("Revisa que todos los campos esten correctos!")
+    }
+    console.log(comands)
+  }
+
 }
