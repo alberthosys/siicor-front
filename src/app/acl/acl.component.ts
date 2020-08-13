63cd981da65f06caf1926b4444eeec16a6310167
alberthosys <alberthosystemas@gmail.com>
2020-08-13 13:57:27 -0500

    13082020

--- src/app/acl/acl.component.ts
+++ src/app/acl/acl.component.ts
@@ -40,7 +40,7 @@ export class AclComponent implements OnInit {
   ) {
     this.formAclEstandar = formBuilder.group({
       ipEntrada: [null, Validators.compose([Validators.required, Validators.pattern('((^|\\.)((25[0-5]_*)|(2[0-4]\\d_*)|(1\\d\\d_*)|([1-9]?\\d_*))){4}_*$')])],
-      mask: [null, Validators.compose([Validators.required, Validators.pattern('((^|\\.)((25[0-5]_*)|(2[0-4]\\d_*)|(1\\d\\d_*)|([1-9]?\\d_*))){4}_*$')])],
+      mask: [null, Validators.compose([Validators.pattern('((^|\\.)((25[0-5]_*)|(2[0-4]\\d_*)|(1\\d\\d_*)|([1-9]?\\d_*))){4}_*$')])],
       group: [null, Validators.compose([Validators.required, Validators.pattern('[0-9]*'), Validators.min(1), Validators.max(99)])],
       cableEntrada: [null, Validators.compose([Validators.required])]
     });
@@ -58,10 +58,12 @@ export class AclComponent implements OnInit {
       group: [null, Validators.compose([Validators.required, Validators.pattern('[0-9]*'), Validators.min(100)])]
     });
   }
-  public clear(){
+
+  public clear() {
     this.formAclEstandar.controls.ipEntrada.setValue(null);
     this.formAclEstandar.controls.group.setValue(null);
     this.formAclEstandar.controls.cableEntrada.setValue(null);
+    this.formAclEstandar.controls.mask.setValue(null);
     this.formAclExtendida.controls.ipEntrada.setValue(null);
     this.formAclExtendida.controls.mask.setValue(null);
     this.formAclExtendida.controls.protocolo.setValue(null);
@@ -72,38 +74,40 @@ export class AclComponent implements OnInit {
     this.formAclExtendida.controls.puerto.setValue(null);
     this.formEliminar.controls.group.setValue(null);
   }
-  public recargar(){
+
+  public recargar() {
     this.ngOnInit();
   }
+
   ngOnInit() {
     this.clear();
     this.checksesion();
-    this.listClables=[];
-    this.listACLlist=[];
-    this.listACLlist2=[];
-    this.listProcolos=[];
+    this.listClables = [];
+    this.listACLlist = [];
+    this.listACLlist2 = [];
+    this.listProcolos = [];
 
-    this.api.consultar(URLServer.seguridad,'').subscribe((response:any)=>{
-      console.log("RESP-ACC->",response)
+    this.api.consultar(URLServer.seguridad, '').subscribe((response: any) => {
+      console.log('RESP-ACC->', response);
       if (response.respuesta.estado === 'success') {
         response.respuesta.interfaces.forEach((int) => {
           this.listClables.push(int);
         });
-        response.respuesta.standar.forEach((standar)=>{
-          standar=standar.replace("\t","");
-          standar=standar.replace("\n","");
-          standar=standar.trim();
+        response.respuesta.standar.forEach((standar) => {
+          standar = standar.replace('\t', '');
+          standar = standar.replace('\n', '');
+          standar = standar.trim();
           this.listACLlist.push(standar);
-        })
-        response.respuesta.extended.forEach((extended)=>{
-          extended=extended.replace("\t","");
-          extended=extended.replace("\n","");
-          extended=extended.trim();
+        });
+        response.respuesta.extended.forEach((extended) => {
+          extended = extended.replace('\t', '');
+          extended = extended.replace('\n', '');
+          extended = extended.trim();
           this.listACLlist2.push(extended);
-        })
+        });
       }
 
-    })
+    });
 
     this.listProcolos.push('tcp');
     this.listProcolos.push('udp');
@@ -123,8 +127,16 @@ export class AclComponent implements OnInit {
   guardar() {
     let listComands: string[] = [];
     if (this.formAclEstandar.valid) {
-      listComands.push(this.comandos.access_list + this.formAclEstandar.controls.group.value + ' ' + (this.permit_deny ? 'permit' : 'deny') + this.comandos.host + this.formAclEstandar.controls.ipEntrada.value+ " "+ this.formAclEstandar.controls.mask.value);
-      // listComands.push(this.comandos.access_list + this.formAclEstandar.controls.group.value + this.comandos.permit_any);
+      let mask = this.formAclEstandar.controls.mask.value;
+      if (mask) {
+        mask = mask.replace(' ', '');
+        mask = mask.trim();
+      }
+      if (mask == null || mask == '') {
+        listComands.push(this.comandos.access_list + this.formAclEstandar.controls.group.value + ' ' + (this.permit_deny ? 'permit' : 'deny') + this.comandos.host + this.formAclEstandar.controls.ipEntrada.value);
+      } else {
+        listComands.push(this.comandos.access_list + this.formAclEstandar.controls.group.value + ' ' + (this.permit_deny ? 'permit ' : 'deny ') + this.formAclEstandar.controls.ipEntrada.value + ' ' + mask);
+      }
       listComands.push(this.comandos.int + this.formAclEstandar.controls.cableEntrada.value);
       listComands.push(this.comandos.ip_acess_group + this.formAclEstandar.controls.group.value + ' ' + (this.in_out ? 'out' : 'in'));
       listComands.push(this.comandos.exit);
@@ -174,9 +186,9 @@ export class AclComponent implements OnInit {
       }
       let protocolo = this.formAclExtendida.controls.protocolo.value;
       if (protocolo.toLowerCase().includes('tcp') || protocolo.toLowerCase().includes('udp')) {
-        comands.push(this.comandos.access_list + this.formAclExtendida.controls.group.value + (this.permit_deny2 ? ' permit ' : ' deny ') + this.formAclExtendida.controls.protocolo.value + ' ' + this.formAclExtendida.controls.ipEntrada.value + ' ' + wildcard + " " + this.formAclExtendida.controls.ipDestino.value + ' ' + (widlcard2 ? widlcard2 + ' ' : '') + ' eq ' + this.formAclExtendida.controls.puerto.value);
+        comands.push(this.comandos.access_list + this.formAclExtendida.controls.group.value + (this.permit_deny2 ? ' permit ' : ' deny ') + this.formAclExtendida.controls.protocolo.value + ' ' + this.formAclExtendida.controls.ipEntrada.value + ' ' + wildcard + ' ' + this.formAclExtendida.controls.ipDestino.value + ' ' + (widlcard2 ? widlcard2 + ' ' : '') + ' eq ' + this.formAclExtendida.controls.puerto.value);
       } else {
-        comands.push(this.comandos.access_list + this.formAclExtendida.controls.group.value + (this.permit_deny2 ? ' permit ' : ' deny ') + this.formAclExtendida.controls.protocolo.value + ' ' + this.formAclExtendida.controls.ipEntrada.value+' '+wildcard + " "+ this.formAclExtendida.controls.ipDestino.value + ' ' + (widlcard2 ? widlcard2 + ' ' : ''));
+        comands.push(this.comandos.access_list + this.formAclExtendida.controls.group.value + (this.permit_deny2 ? ' permit ' : ' deny ') + this.formAclExtendida.controls.protocolo.value + ' ' + this.formAclExtendida.controls.ipEntrada.value + ' ' + wildcard + ' ' + this.formAclExtendida.controls.ipDestino.value + ' ' + (widlcard2 ? widlcard2 + ' ' : ''));
       }
       comands.push(this.comandos.int + this.formAclExtendida.controls.cableEntrada.value);
       comands.push(this.comandos.ip_acess_group + this.formAclExtendida.controls.group.value + ' ' + (this.in_out2 ? 'out ' : 'in '));
@@ -204,18 +216,18 @@ export class AclComponent implements OnInit {
   eliminar(acl: string) {
     alertConfirm.fire({html: 'Se eliminarán todas las sentencias asociadas a esta lista de acceso. ¿Está seguro que desea eliminar este grupo?'}).then((response) => {
       if (response.value) {
-        let aclTemp:string[]=acl.split(" ");
-        let comand: string = 'no ' + aclTemp[0]+"-"+aclTemp[1]+" "+aclTemp[2];
+        let aclTemp: string[] = acl.split(' ');
+        let comand: string = 'no ' + aclTemp[0] + '-' + aclTemp[1] + ' ' + aclTemp[2];
         console.log('ELiminar->' + comand);
-        if(!this.ventana){
+        if (!this.ventana) {
           this.globalComands.push(comand);
-        }else{
+        } else {
           this.globalComands2.push(comand);
         }
         this.alerta.alertSuccess('Se ha eliminado exitosamente !');
-        this.api.consultar(URLServer.envioDatos,comand).subscribe((response)=>{
-          console.log("ResponseDeleteACL->",response)
-        })
+        this.api.consultar(URLServer.envioDatos, comand).subscribe((response) => {
+          console.log('ResponseDeleteACL->', response);
+        });
       }
     });
     this.ngOnInit();
